24cfb98aa23a6f6bcfe647c63a75cc0f9aa281be
Lucid <lucidprogrammer@hotmail.com>
2019-12-31 12:49:32 +0000

    hooks

--- src/web1/index.js
+++ src/web1/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import sleep from 'sleep';
 const app = express();
 
-let port = 3000;
-const revision = `Revision is ${process.env.REVISION} and namespace is ${process.env.NAMESPACE}`;
+
+const msleep = (n) => Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+const sleep = (n) => msleep(n*1000);
+  
+
+const port = process.env.PORT? process.env.PORT: 3000;
+const message = `Revision is ${process.env.REVISION?process.env.REVISION:"unknown"} and namespace is ${process.env.NAMESPACE?process.env.NAMESPACE:"unknown"}`;
 app.get('/', (req, res) => {
-    sleep.sleep(2); //mimic an intensive operation to showcase autoscaling
-    return res.send(`Hello World ! ${revision}`);
+    sleep(2); //mimic an intensive operation to showcase autoscaling
+    return res.send(`Hello World ! ${message}`);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}! ${revision}`))
+app.listen(port, () => console.log(`Example app listening on port ${port}! ${message}`))
