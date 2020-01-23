64ada50ed4d7dc35ed6c9d9fef75fab6b21cea04
Lucid <lucidprogrammer@hotmail.com>
2020-01-23 11:51:35 +0000

    http2

--- src/web1/index.js
+++ src/web1/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
+const spdy = require('spdy');
 const app = express();
 app.use(express.json());
 
 const msleep = (n) => Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
-const sleep = (n) => msleep(n*1000);
+const sleep = (n) => msleep(n * 1000);
 
-const port = process.env.PORT? process.env.PORT: 3000;
+const port = process.env.PORT ? process.env.PORT : 3000;
 
 
 
 app.get('*', (req, res) => {
-    if(process.env.SLEEP){
+    if (process.env.SLEEP) {
         sleep(process.env.SLEEP); //mimic an intensive operation to showcase autoscaling
     }
-    const application = req.path.replace('/','');
+    const application = req.path.replace('/', '');
     const message = `
     Welcome to ${application ? application : 'sample'} app
     Revision is ${process.env.REVISION ? process.env.REVISION : "unknown"} 
@@ -22,16 +23,23 @@ app.get('*', (req, res) => {
     return res.send(`${message}`);
 });
 
-app.post('*', (req,res)=>{
-     if(req.body.sleep){
-        sleep(req.body.sleep); 
+app.post('*', (req, res) => {
+    if (req.body.sleep) {
+        sleep(req.body.sleep);
     }
     res.send({
         status: 200
     });
 })
 
-app.listen(port, () => console.log(`
+spdy.createServer({
+    spdy: {
+        protocols: ['h2'],
+        ssl: false,
+        plain: true,
+        'x-forwarded-for': true,
+    }
+}, app).listen(port, '0.0.0.0', () => console.log(`
 Example app listening on port ${port}! 
 Revision is ${process.env.REVISION ? process.env.REVISION : "unknown"} 
 Namespace is ${process.env.NAMESPACE ? process.env.NAMESPACE : "unknown"}
